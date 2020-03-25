0f8b88a81ec96d465fa6f9faf1ecdb7a5a70fa50
imdhemy <imdhemy@gmail.com>
2020-03-25 05:44:00 +0200

    Scaffold wordpress tests

---
+++ tests/test-sample.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Alexander
+ */
+
+/**
+ * Sample test case.
+ * @group sample
+ */
+class SampleTest extends WP_UnitTestCase
+{
+
+    /**
+     * A single example test.
+     */
+    public function test_sample()
+    {
+        // Replace this with some actual testing code.
+        $this->assertTrue(true);
+    }
+}
