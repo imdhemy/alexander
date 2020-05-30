92390515c62b71c3663e0b82212f84f15ea5542d
imdhemy <imdhemy@gmail.com>
2020-05-30 18:11:13 +0200

    Create console command to serve the application on the PHP development server

---
+++ components/alexander-alex/src/Commands/RunBuiltInServerCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Macedonia\Alex\Commands;
+
+
+use Macedonia\Alex\Command;
+use Symfony\Component\Process\Process;
+use function dd;
+use function shell_exec;
+
+/**
+ * Class RunBuiltInServerCommand
+ * @package Macedonia\Alex\Commands
+ */
+class RunBuiltInServerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "serve";
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Serve the application on the PHP development server';
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(): void
+    {
+       $root_directory = "./tmp/wordpress";
+       $this->info("Alexander development server started: http://localhost:8080");
+       $output = shell_exec("php -S localhost:8080 -t $root_directory");
+       $this->newLine($output);
+    }
+}
